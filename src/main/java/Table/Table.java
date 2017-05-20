2db6901202bf3316baa0d99031e45a018add9922
Brian Mullin <brianmullin@zipcoder6.local>
2017-05-19 20:30:08 -0400

    forgot to commit earlier, forgot what I did

--- src/main/java/Table/Table.java
+++ src/main/java/Table/Table.java
@@ -4,7 +4,7 @@ import java.util.ArrayList;
 
 /**
  * This class needs to manage an ArrayList of Entry objects.  It needs a get method that takes a key and returns
- * its corresponding value, or null of the key is not in the arraylist.  It needs a put method that takes a key and value
+ * its corresponding value, or null if the key is not in the arraylist.  It needs a put method that takes a key and value
  * and makes an entry with key, value.  NOTE: There cannot be two entries with the same key.
  * It also needs a remove method which takes a key and, if that key exists in the arraylist, removes that item.
  * Void return on `remove`.
@@ -13,5 +13,41 @@ public class Table<K, V> {
     private ArrayList entries;
 
     public Table() {
+
     }
+
+    public V get(K key){
+        for(int i = 0; i < entries.size(); i++){
+            Entry holder = (Entry) entries.get(i);
+
+            if (key.equals(holder.getKey())){
+                return (V) holder.getValue();
+            }
+        }
+        return null;
+    }
+
+    public void put(K key, V value){
+        for(int i = 0; i < entries.size(); i++){
+            Entry holder = (Entry) entries.get(i);
+
+            if (key.equals(holder.getKey())){
+                break;
+            }
+            else {
+                entries.push(key, value);
+            }
+
+        }
+
+
+    }
+
+    public void remove(K key){
+
+    }
+
+
+
+
 }
